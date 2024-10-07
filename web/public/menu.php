1216d460b16748d8b699ff854350a92a66ab8415
JOSE SOSA <joseavelinomoreno@gmail.com>
2024-10-07 15:47:17 -0500

    Delete web directory

--- web/public/menu.php
+++
@@ -1,6 +0,0 @@
-<nav class="nav nav-masthead justify-content-center float-md-end">
-    <a class="nav-link fw-bold py-1 px-2 active" aria-current="page" href="registrar.php">REGISTRAR PERSONA</a>
-    <a class="nav-link fw-bold py-1 px-2 active" aria-current="page" href="matricular.php">MATRICULAR PROGRAMA</a>
-    <a class="nav-link fw-bold py-1 px-2 active" aria-current="page" href="iniciar.php">INICIAR SESION</a>
-</nav>
-    
