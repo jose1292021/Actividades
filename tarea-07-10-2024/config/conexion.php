5fdd2a2e0cea8931100582965888910dac9a1b7a
JOSE SOSA <joseavelinomoreno@gmail.com>
2024-10-07 15:50:29 -0500

    Add files via upload

---
+++ tarea-07-10-2024/config/conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+$dataBase = "mysql:dbname=registro;host=127.0.0.1";
+$user = "root";
+$pass = "";
+try {
+    $conexion = new PDO($dataBase, $user, $pass);
+}
+catch (Exception $e) {
+    die("Error Al Conectar Con La Base De Datos");
+}
+
+
+?>
